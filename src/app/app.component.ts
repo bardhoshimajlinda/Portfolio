1b98d1da848f5aae3978f5b185fa6a2e5c3af131
bardhoshimajlinda <116483572+bardhoshimajlinda@users.noreply.github.com>
2024-02-24 23:38:29 +0100

    Added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {NavbarComponent} from "./navbar/navbar.component";
+import {HomeComponent} from "./home/home.component";
+import {AboutComponent} from "./about/about.component";
+import {ProjectsComponent} from "./projects/projects.component";
+import {ResumeComponent} from "./resume/resume.component";
+import {ContactComponent} from "./contact/contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, RouterLink, NavbarComponent, RouterLinkActive, HomeComponent,AboutComponent, ProjectsComponent, ResumeComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import {NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {filter} from "rxjs";
+import {HomeComponent} from "../home/home.component";
+import {AboutComponent} from "../about/about.component";
+import {ProjectsComponent} from "../projects/projects.component";
+import {ResumeComponent} from "../resume/resume.component";
+import {ContactComponent} from "../contact/contact.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HomeComponent,AboutComponent, ProjectsComponent, ResumeComponent, ContactComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  activeRoute: string = '';
 }
